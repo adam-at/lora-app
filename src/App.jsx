a58893ee04219bd415c7aeef41e3ee6bb0258924
adam.aliouat <adam.aliouat@telecom-sudparis.eu>
2022-06-30 12:42:29 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.css';
 import React from 'react';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 function App() {
   return (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
 import {Link} from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function Navbar() {
   const [click, setClick] = useState(false);
   return (
     <>
         <nav classname="navbar">
+        <FontAwesomeIcon icon="fa-solid fa-book" />
             <div class="navbar-container">
                 <Link to="/" classname="navbar-logo">
-                    TRVL <i class="fa fa-address-book"></i>
+                    TRV
+                    <FontAwesomeIcon icon="fa-solid fa-book"/>
                 </Link>
                 <div className="menu-icon">
                   <i className={click ? 'fas fa-alarm-plus' : 'fas fa-alarm-exclamation'}/>
